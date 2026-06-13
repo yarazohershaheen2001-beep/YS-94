84e80ce0064c922994355ce34c35cfab7decb11d
yarazohershaheen2001-beep <yarazohershaheen2001@gmail.com>
2026-06-13 08:53:13 +0300

    chore: add keyboards/__init__.py

---
+++ keyboards/__init__.py
@@ -0,0 +1,33 @@
+from .main import (
+    subscription_keyboard,
+    main_menu_keyboard,
+    developer_keyboard,
+    platforms_keyboard,
+    payment_choice_keyboard,
+    payment_methods_keyboard,
+    admin_action_keyboard,
+)
+from .packages import (
+    tiktok_packages_keyboard,
+    instagram_packages_keyboard,
+    facebook_packages_keyboard,
+    telegram_packages_keyboard,
+    snapchat_packages_keyboard,
+    youtube_packages_keyboard,
+)
+
+__all__ = [
+    "subscription_keyboard",
+    "main_menu_keyboard",
+    "developer_keyboard",
+    "platforms_keyboard",
+    "payment_choice_keyboard",
+    "payment_methods_keyboard",
+    "admin_action_keyboard",
+    "tiktok_packages_keyboard",
+    "instagram_packages_keyboard",
+    "facebook_packages_keyboard",
+    "telegram_packages_keyboard",
+    "snapchat_packages_keyboard",
+    "youtube_packages_keyboard",
+]
